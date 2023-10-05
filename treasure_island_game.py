d92778413dcb4124ae1cf81a954991b387bcfa01
LakshitaGaneshKumar <146595218+LakshitaGaneshKumar@users.noreply.github.com>
2023-10-05 15:11:59 -0400

    Treasure Island Game

---
+++ treasure_island_game.py
@@ -0,0 +1,51 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+direction = input(
+    "You are at a crossroad. Which way do you want to go, left or right? L or R? "
+)
+if direction == "L":
+    swim_wait = input(
+        "You are faced by a vast river with rabid waters. Do you want to swim or wait? S or W? "
+    )
+    if swim_wait == "W":
+        door = input(
+            "Now choose your destiny. Which door will you go through: red, yellow, or blue? R, Y, or B? "
+        )
+        if door == "Y":
+            print("You win!")
+        elif door == "R":
+            print("You were burned by fire. Game Over.")
+        elif door == "B":
+            print("You were eaten by beasts. Game Over.")
+        else:
+            print("Game Over.")
+    else:
+        print("You were attacked by trout. Game Over.")
+else:
+    print("You fell into a hole. Game Over.")
